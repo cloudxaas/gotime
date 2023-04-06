ec8f665930d36a7d38ba66726fe383936f2530cc
Kol Influence <104323920+kolinfluence@users.noreply.github.com>
2023-04-06 16:07:42 -0700

    Update time.go

--- time.go
+++ time.go
@@ -12,4 +12,17 @@ func NanoNowBytes() []byte {
 	return cxbytes.Uint64ToBytes(NanoNow())
 }
 
+func GetCurrentYear() []byte {
+    now := time.Now()
+    year := now.Year()
+    if year < 1000 {
+        return []byte{'0', '0', '0', '0'}
+    }
+    var buf [4]byte
+    for i := 3; i >= 0; i-- {
+        buf[i] = byte('0' + year%10)
+        year /= 10
+    }
+    return buf[:]
+}
 
